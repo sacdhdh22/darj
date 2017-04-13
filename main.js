3471b963d0b0eb64321ba20307ef4362aec17ee9
sachin <sachin@zoyo.co.in>
2017-04-13 18:09:42 +0530

    adding minor changes

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'free-template')));
+app.use(express.static(path.join(__dirname, 'public')));
 //page to be loaded when server runs
 app.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname,'free-template', 'index.html'));
+});
+app.get('/sikkim', function(req, res, next) {
+console.log("we are here");
+  res.sendFile(path.join(__dirname,'free-template', 'views', 'sikkim.html'));
+
+});
+
+app.get('/darjeeling', function(req, res, next) {
+  res.sendFile(path.join(__dirname,'free-template', 'views', 'darjeeling.html'));
 });
 app.listen(3000);
